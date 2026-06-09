ba80f8b92997c7f4af596eed04420c4a24a3106b
Carlo <carlofernandoflores2006@gmail.com>
2026-06-09 16:28:18 -0600

    prueba3

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,4 @@ Route::get('/quiz',    fn() => view('quiz.index'))->name('quiz');
 Route::get('/casas',   fn() => view('casas.index'))->name('casas');
 Route::get('/dominios',fn() => view('dominios'))->name('dominios');
 Route::get('/recorrido',fn() => view('recorrido'))->name('recorrido');
-Route::get('/welcome',fn() => view('welcome'))->name('welcome');
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('welcome'))->name('home');
