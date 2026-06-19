75d2d97a19e400b5eb2a67a40254703ae38e6b62
Carlo <carlofernandoflores2006@gmail.com>
2026-06-19 02:46:16 -0600

    correccion al hero pq no se ve

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 
     {{-- Imagen de fondo --}}
     <div style="position:absolute;inset:0;
-                background-image:url('{{ asset('images/hero-leon.png') }}');
+                background-image:url('{{ asset('imagenes/hero-leon.png') }}');
                 background-size:cover;
                 background-position:right center;">
     </div>
