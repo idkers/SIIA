5df45c7166d99e0a16ae0bc6194be917735829a5
Carlo <carlofernandoflores2006@gmail.com>
2026-07-01 18:46:18 -0600

    CAMBIO A garrita 3

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -966,7 +966,7 @@
                  style="background:radial-gradient(circle at top right,
                             rgba(232,201,106,.25) 0%,
                             rgba(200,168,75,.12) 20%,
-                            transparent 45%),
+                            transparent 90%),
                         linear-gradient(135deg,#06060F 0%,#120D08 30%,#1A1208 60%,#06060F 100%);
                         border:1px solid rgba(200,168,75,.35);
                         border-radius:10px;
