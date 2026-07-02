90d58a7c1e088b5c80b6da46b41b445326379095
Carlo <carlofernandoflores2006@gmail.com>
2026-07-01 18:48:49 -0600

    CAMBIO A garrita 4

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -963,12 +963,7 @@
 <div id="stage-3" class="stage" style="display:none;">
     <div class="stage-wrap">
         <section id="stage-3-inner"
-                 style="background:radial-gradient(circle at top right,
-                            rgba(232,201,106,.25) 0%,
-                            rgba(200,168,75,.12) 20%,
-                            transparent 90%),
-                        linear-gradient(135deg,#06060F 0%,#120D08 30%,#1A1208 60%,#06060F 100%);
-                        border:1px solid rgba(200,168,75,.35);
+                 style="background:transparent;
                         border-radius:10px;
                         display:flex;
                         flex-direction:column;
