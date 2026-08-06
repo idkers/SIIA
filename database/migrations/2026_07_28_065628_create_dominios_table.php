c690aaf5f06237effc7b2ac20aaf070eedf35c59
Carlo <carlofernandoflores2006@gmail.com>
2026-08-06 01:19:29 -0600

    7

--- database/migrations/2026_07_28_065628_create_dominios_table.php
+++ database/migrations/2026_07_28_065628_create_dominios_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
     $table->string('nombre_casa');
     $table->string('color', 7);
     $table->text('descripcion');
-    $table->string('imagen')->nullable();
     $table->timestamps();
 });
     }
